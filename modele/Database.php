0be1a4c5596de126e42391985fd82d3bc4bd7e33
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-03-19 19:40:56 +0100

    first push

---
+++ modele/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+class Database
+{
+    private static $dns;
+    private static $user;
+    private static $password;
+    private static $database;
+
+    public function __construct()
+    {
+        self::$dns = "mysql:host=localhost;dbname=leboncoin;port=3306"; // À changer selon vos configurations
+        self::$user = "root"; // À changer selon vos configurations
+        self::$password = ""; // À changer selon vos configurations
+        self::$database = new PDO(self::$dns, self::$user, self::$password);
+    }
+}
