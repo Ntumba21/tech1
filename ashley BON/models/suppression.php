b847ec55d111807fad3d9fd4e2c59b23eea86e39
ashley0801 <117387197+ashley0801@users.noreply.github.com>
2023-03-21 22:44:51 +0100

    user+mail

---
+++ ashley BON/models/suppression.php
@@ -0,0 +1,37 @@
+<?php
+class Model {
+  private $db;
+  
+  function __construct() {
+    try {
+      $this->db = new PDO('mysql:host=' . 'localhost'. ';dbname='. 'projet-tech;' . 'port='. '3307', 'root', '');
+      $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+      echo 'Error: ' . $e->getMessage();
+      die();
+    }
+  }
+
+  function setUserInactive($user_id) {
+    $stmt = $this->db->prepare('UPDATE user SET isvalide = 0, inactive_time = NOW() WHERE iduser = ?');
+    $stmt->execute(array($user_id));
+  }
+
+  function setUserActive($user_id) {
+    $stmt = $this->db->prepare('UPDATE user SET isvalide = 1, inactive_time = NULL WHERE iduser = ?');
+    $stmt->execute(array($user_id));
+  }
+
+  function deleteUser($user_id) {
+    $stmt = $this->db->prepare('DELETE FROM user WHERE iduser = ?');
+    $stmt->execute(array($user_id));
+  }
+
+  function getUserByEmail($email) {
+    $stmt = $this->db->prepare('SELECT * FROM user WHERE mail = ?');
+    $stmt->execute(array($email));
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+  }
+  
+}
+?>
