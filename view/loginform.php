bca23a05de6ab0a62a2528e0cdcc01699ddb8248
Naotymn <naomymegane.ymeletsayem@edu.ece.fr>
2023-03-22 23:13:55 +0100

    view/loginform

--- view/loginform.php
+++ view/loginform.php
@@ -1,17 +1,62 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <title>Connexion</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login form</title>
+    <style>
+        body {
+            background-color: #fff;
+            color: #000;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            text-align: center;
+        }
+        form {
+            margin: 0 auto;
+            width: 400px;
+            border: 2px solid #000;
+            padding: 20px;
+        }
+        label {
+            display: block;
+            margin-bottom: 10px;
+        }
+        input[type="email"], input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #000;
+            border-radius: 5px;
+            margin-bottom: 20px;
+        }
+        button[type="submit"] {
+            background-color: #000;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+        button[type="submit"]:hover {
+            background-color: #444;
+        }
+    </style>
 </head>
 <body>
-    <h1>Connexion</h1>
-    
-    <form method="post" action="../controller/login.php">
-        <label for="mail">mail :</label>
-        <input type="mail" id="mail" name="mail" required><br><br>
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <input type="submit" value="Se connecter">
-    </form>
+
+<h1>Connexion</h1>
+
+<form action="../controller/login.php" method="post">
+    <label for="email">Adresse e-mail :</label>
+    <input type="email" name="email" required><br>
+
+    <label for="password">Mot de passe :</label>
+    <input type="password" name="password" required>
+
+    <button type="submit">Se connecter</button>
+</form>
+
 </body>
 </html>
