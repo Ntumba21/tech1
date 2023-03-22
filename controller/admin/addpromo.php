1e963d386d8f34083853bd01139df4aab205ede9
Ntumba21 <115614846+Ntumba21@users.noreply.github.com>
2023-03-22 17:04:30 +0100

    addpromo

---
+++ controller/admin/addpromo.php
@@ -0,0 +1,8 @@
+<?php
+    require_once ('..\..\modele\Database.php');
+    require_once ('..\..\controller\session.php');
+    $data = new Database();
+    $admin = $data->AddPromo();
+    
+       
+?>
