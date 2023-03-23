af79cf6dd7ddcf96ce80e9aff89249f836e9b78c
ashley0801 <117387197+ashley0801@users.noreply.github.com>
2023-03-23 21:40:27 +0100

    arrangement de register

--- controller/promo.php
+++ controller/promo.php
@@ -1,9 +1,8 @@
 <?php
-   require_once '../modele/Database.php';
-    function ShowPromoEleve(){
+    require_once '../modele/Database.php';
+    function ShowPromo(){
         $db = new Database();
         $promos = $db->GetPromos();
-        echo '<select name="idpromos" id="idpromos" required>';
         foreach($promos as $promo){
             echo '<option value="'.$promo['idpromos'].'">'.$promo['nom'].'</option>';
         }
