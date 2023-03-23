9f9031c5fb39b8e8a4d36379e16ac70d43dd3e60
Ntumba21 <115614846+Ntumba21@users.noreply.github.com>
2023-03-23 15:41:54 +0100

    delete user and promo

--- controller/admin/addpromo.php
+++ controller/admin/addpromo.php
@@ -1,8 +1,14 @@
 <?php
     require_once ('..\..\modele\Database.php');
     require_once ('..\..\controller\session.php');
-    $data = new Database();
-    $admin = $data->AddPromo();
-    
-       
+    if (isset($_POST["promos"])){
+        $data = new Database();
+        $admin = $data->AddPromo($_POST["promos"]);
+        //echo $_POST["promos"];
+        echo 'Donnée ajoutée';
+
+    }else{
+        echo 'Donnée pas ajoutée';
+    }
+      
 ?>
