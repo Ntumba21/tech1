85e7bf71b44e2784e080668f92dea541589af175
Manalmell <117366901+Manalmell@users.noreply.github.com>
2023-03-25 05:15:53 +0100

    LOGIN CRYPTE

--- controller/login.php
+++ controller/login.php
@@ -6,9 +6,9 @@ require_once ('..\modele\Database.php');
 if(isset($_POST['email']) && isset($_POST['password'])) {
  $db = new Database();
  $user = $db->Connect($_POST['email'], $_POST['password']);
- if(count($user) > 0) {
+ if(count($user) > 0 && password_verify($_POST['password'], $user[0]['password'])) {
     Session($user[0]["mail"], $user[0]["iduser"], true);
-    redirectToHome(); 
+    //redirectToHome(); 
  $redirectUrl = "../view/profil.php"; // use an absolute path
  echo '<script>window.location.href = "'.$redirectUrl.'";</script>';
  } else {
