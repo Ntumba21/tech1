969eb20d334f099f23d01992183cb38251971132
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-03-27 20:32:45 +0200

    stat+database

--- controller/admin/statuser.php
+++ controller/admin/statuser.php
@@ -31,14 +31,17 @@ function MakeStaForMessagePerDay(){
     }
     echo "<script>
                         const ctx = document.getElementById('myChart');
-
+                        let Label = ".json_encode($date)." ;
+                        let message = ".json_encode($message)." ;
+                        console.log(Label);
+                        console.log(message);
                         new Chart(ctx, {
                             type: 'line',
                             data: {
-                                labels:".json_encode($date).",
+                                labels: Label,
                                 datasets: [{
                                     label: 'Nombre de message par mois',
-                                    data: ".json_encode($message).",
+                                    data: message,
                                     borderWidth: 1
                                 }]
                             },
