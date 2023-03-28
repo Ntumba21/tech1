b05aef5450dafb0c838a2568b6a0e90a0c8003f5
Naotymn <naomymegane.ymeletsayem@edu.ece.fr>
2023-03-29 00:30:13 +0200

    il reste a centrer les pages sur view et faire admin

--- view/admin/create-post.php
+++ view/admin/create-post.php
@@ -8,18 +8,43 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../../view/style/post.css">
+    <link rel="stylesheet" href="../../facebookk/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
-    <section class="createPost">
-
 <header>
-    <img src="../../media/logo ECEBOOK.png" alt="Logo" class="logo"> 
-    <h1>Create post Admin</h1>
-</header>
-<div class="content">
-    <div class="square">
+        <div class="header-container">
+            <div class="header-wrapper">
+                <div class="logoBox">
+                    <img src="../../media/logo ECEBOOK.png" alt="logo">
+                </div>
+                <div class="searchBox">
+                    <input type="search">
+                    <i class="fas fa-search"></i>
+                </div>
+                <div class="iconBox2">
+                <i class="fa-solid fa-house"></i>
+                    <i class="fa-solid fa-bell"></i>
+                    <label>  <a href="../../facebookk/profil.php">
+                    <img src="../../facebookk/images/us2.png" alt="user">
+                     </label></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="home">
+    <div class="container">
+        <div class="home-weapper">
+
+           <!--GAUCHE-->
+            <div class="home-left">
+                 <!-- BON-->
+  
+                 <div class="createPost">
+    <h3 class="mini-headign">Create post Admin</h3> 
+  
     <form name="ajouter-post" method="post" action="../../controller/admin/create-post.php" enctype="multipart/form-data">
         <input type="hidden" name="action" value="create">
         <label for="type">Type :</label>
