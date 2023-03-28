47a71ad15f67b5d3c31c28099eadd73efdf29b23
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-03-28 10:00:20 +0200

    create-admin.php est dait

--- controller/admin/connect.php
+++ controller/admin/connect.php
@@ -1,9 +1,13 @@
 <?php
     require_once ('..\..\modele\Database.php');
     require_once ('..\..\controller\session.php');
+
     if (isset($_POST['mail'])&& $_POST['password']){
         $data = new Database();
-        $admin = $data->ConnectAdmin($_POST['mail'], $_POST['password']);
+        //decryptage du mot de passe
+        $password = $_POST['password'];
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $admin = $data->ConnectAdmin($_POST['mail'], $password);
         if (count($admin) >0){
            Session($admin[0]["mail"],$admin[0]["password"],true);
            redirectToHomeAdmin();
--- controller/admin/postfunction.php
+++ controller/admin/postfunction.php
@@ -1,4 +1,5 @@
 <?php
+require_once ('..\..\modele\Database.php');
 function ShowAllPost(): void
 {
     $data = new Database();
