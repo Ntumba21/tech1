72300ab33f27a4a27bf5ec56a207b06400dbe563
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-03-28 21:02:29 +0200

    mail is good

--- controller/admin/deleteuser.php
+++ controller/admin/deleteuser.php
@@ -1,6 +1,7 @@
 <?php
     require_once ('..\..\modele\Database.php');
     require_once ('..\..\controller\session.php');
+    require_once 'mail-function.php';
     $data = new Database();
     if(isset($_POST['submit'])){
         var_dump($_POST);
@@ -8,6 +9,9 @@
             $result = $data->DeleteUserById($iduser);
         }
         if($result){
+            $message = "Bonjour, votre compte a été supprimé par l'administrateur du site EceBook. Pour plus d'informations, veuillez contacter l'administrateur du site.";
+            $sujet = "Suppression de votre compte EceBook";
+            AdminMail($_POST['email'],$sujet,$message);
             echo $_SESSION['alert'] = "Suppression réussie";
         }else{
             echo $_SESSION['alert']="Suppression échouée";
