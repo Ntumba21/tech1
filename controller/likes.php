a39415a663dd311ae77f8915e067e3e424269b90
Wildrey237 <wilfriedbemelingue@gmail.com>
2023-03-28 03:27:13 +0200

    j'ai fait les likes.php

---
+++ controller/likes.php
@@ -0,0 +1,15 @@
+<?php
+require_once require_once ('..\controller\session.php ');
+require_once ('..\modele\Database.php');
+function like($mail,$idpost){
+    $like = 1;
+    $data = new Database();
+    $data->likes($like,$mail,$idpost);
+
+}
+function dislike($mail,$idpost){
+    $like = 2;
+    $data = new Database();
+    $data->likes($like,$mail,$idpost);
+
+}
