f5925ea6afaa6bd4b14903c023a427b0e5b59276
Ntumba21 <115614846+Ntumba21@users.noreply.github.com>
2023-03-29 12:57:15 +0200

    decriptage user ok

--- controller/login.php
+++ controller/login.php
@@ -4,17 +4,16 @@ require_once ('..\modele\Database.php');
  
 
 if(isset($_POST['email']) && isset($_POST['password'])) {
- $db = new Database();
- $user = $db->Connect($_POST['email'], $_POST['password']);
- if(count($user) > 0) {
-    Session($user[0]["mail"], $user[0]["iduser"], true);
-    //redirectToHome(); 
- $redirectUrl = "../facebookk/index.php"; // use an absolute path
- echo '<script>window.location.href = "'.$redirectUrl.'";</script>';
- } else {
-    $_SESSION['alert'] = 'Identifiants incorrects ';
-     $_SESSION['redirection'] = 'loginform.php';
-     header('Location: ../view/alert.php');
- }
+   $db = new Database();
+   $user = $db->Connect($_POST['email'], $_POST['password']);
+   if($user) {
+       Session($user["mail"], $user["iduser"], true);
+       $redirectUrl = "../facebookk/index.php"; // use an absolute path
+       echo '<script>window.location.href = "'.$redirectUrl.'";</script>';
+   } else {
+       $_SESSION['alert'] = 'Identifiants incorrects ';
+       $_SESSION['redirection'] = 'loginform.php';
+       header('Location: ../view/alert.php');
+   }
 }
 
