72b5afc54a482669f1b1a42376bee6f8f1332a98
Naotymn <naomymegane.ymeletsayem@edu.ece.fr>
2023-03-29 21:34:28 +0200

    je suis entrain d'arranger les css

--- view/profil.php
+++ view/profil.php
@@ -16,52 +16,10 @@ $user = $database->getUserByEmail($mail);
   <head>
     <meta charset="UTF-8">
     <title>Profil </title>
-    <link rel="stylesheet" href="../view/style/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-<body>
-<header>
-    <div class="header-container">
-        <div class="header-wrapper">
-        <?php $user = $db->getUserByEmail($_SESSION['mail']);?>
-            <div class="logoBox">
-                <img src="../media/logo ECEBOOK.png" alt="logo">
-            </div>
-            <div class="searchBox">
-                <input type="search">
-                <i class="fas fa-search"></i>
-            </div>
-            <div class="iconBox2">
-                <!-- Lien vers index.php avec l'icône de la maison -->
-                <a href="../facebookk/index.php">
-                    <i class="fa-solid fa-house"></i>
-                </a>
-                <i class="fa-solid fa-bell"></i>
-                <a href="../facebookk/profil.php">
-                    <img src="<?php echo $user['photo'] ?>"  alt="user">
-                </a>
-                <!-- Bouton de déconnexion -->
-                <a href="logout.php" class="logout-btn">
-                    <i class="fas fa-sign-out-alt"></i> 
-                </a>
-            </div>
-        </div>
-    </div>
-</header>
-
-
-    <div class="home">
-    <div class="container">
-        <div class="home-weapper">
-
-           <!--GAUCHE-->
-            <div class="home-left">
-                 <!-- BON-->
+    <link rel="stylesheet" href="../view/style/post.css">
+   
+    </head>
   
-    
-    <!-- <div class="createPost"> -->
-    <h1 class="mini-headign">Inscription</h1>
-  </head>
   <body>
     <h1>Profil de <?php echo $user["prenom"] . " " . $user["nom"]; ?></h1>
     
