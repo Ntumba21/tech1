a701aa21da1844346b74365cd9561e4c6dfeb206
ashley0801 <117387197+ashley0801@users.noreply.github.com>
2023-03-30 00:02:18 +0200

    changement

--- facebookk/index.php
+++ facebookk/index.php
@@ -20,7 +20,7 @@ $data = webscraping();
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
     <title>facebook.com</title>
     <!-- style css link -->
-    <link rel="stylesheet" href="../view/style/index.css">
+    <link rel="stylesheet" href="style.css">
     <!-- fontawesome css link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
