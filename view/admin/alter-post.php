d4b05e3ec1dd01a171de806d4f34e63b93cfc3fb
Naotymn <naomymegane.ymeletsayem@edu.ece.fr>
2023-03-29 12:38:48 +0200

    register ok

--- view/admin/alter-post.php
+++ view/admin/alter-post.php
@@ -10,12 +10,48 @@ $lieu = $data->getLieuByIdPost($_GET['idpost']);
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <link rel="stylesheet" href="../../view/style/manage.css">
-</html>
+    <link rel="stylesheet" href="../../view/style/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
 <body>
+<header>
+        <div class="header-container">
+            <div class="header-wrapper">
+                <div class="logoBox">
+                    <img src="../../media/logo ECEBOOK.png" alt="logo">
+                </div>
+                <div class="searchBox">
+                    <input type="search">
+                    <i class="fas fa-search"></i>
+                </div>
+                <div class="iconBox2">
+                <i class="fa-solid fa-house"></i>
+                    <i class="fa-solid fa-bell"></i>
+                    <label>  <a href="../../facebookk/profil.php">
+                    <img src="../../facebookk/images/us2.png" alt="user">
+                     </label></a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+    <div class="home">
+    <div class="container">
+        <div class="home-weapper">
+
+           <!--GAUCHE-->
+            <div class="home-left">
+                 <!-- BON-->
+  
+                 <h1> Modifier un post </h1>
+    </header>
+    <div class="content">
+        <div class="square">
+        <section>
     <main>
         <section>
-            <h1> Modifier un post </h1>
+            
             <form action="../../controller/admin/alter-post.php" method="post" enctype="multipart/form-data">
                 <input type="hidden" name="id-post" value="<?php echo $_GET['idpost']; ?>">
                 <label for="type">Type</label>
